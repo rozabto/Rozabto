a23663ee8327ef48de493a0e3cc700c1a16bbe28
destbg <35627380+destbg@users.noreply.github.com>
2019-02-09 18:25:29 +0200

    0

--- Rozabto/MainWindow.xaml.cs
+++ Rozabto/MainWindow.xaml.cs
@@ -14,9 +14,6 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Rozabto {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
