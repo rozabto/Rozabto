d69b41052a8ecf99f7e7b0fbc06963962878f613
simona1707 <35624866+simona1707@users.noreply.github.com>
2019-02-16 15:46:52 +0200

    16.02

--- Rozabto/View/Playlists.xaml.cs
+++ Rozabto/View/Playlists.xaml.cs
@@ -24,5 +24,15 @@ namespace Rozabto.View
         {
             InitializeComponent();
         }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
