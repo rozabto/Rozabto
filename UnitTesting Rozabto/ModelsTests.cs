db4a7e5dffdcc396e8fed4427f9da00a48ad05b3
NanislavaPavlova <35624878+NanislavaPavlova@users.noreply.github.com>
2019-04-01 14:34:30 +0300

    ......

---
+++ UnitTesting Rozabto/ModelsTests.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using MaterialDesignThemes.Wpf;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rozabto.Model;
+using Rozabto.ViewModel;
+
+namespace UnitTesting_Rozabto
+{
+    [TestClass]
+    public class ModelsTests
+    {
+        [TestMethod]
+        public void TestMethod_Album()
+        {
+            var album = new Album("Album_Namee");
+            Assert.AreEqual(album.Name, "Album_Namee");
+            album = new Album("Name", new System.Collections.Generic.List<Song>());
+
+        }
+
+        [TestMethod]
+        public void TestMethod_AlbumEF()
+        {
+            var album = new AlbumEF();
+
+            album.ID = 42;
+            album.Name = "Prodigy";
+
+            Assert.AreEqual(album.ID, 42);
+            Assert.AreEqual(album.Name, "Prodigy");
+        }
+
+
+        [TestMethod]
+        public void TestMethod_AlbumSongsEF()
+        {
+            var album = new AlbumSongsEF();
+
+            album.ID = 42;
+            album.AlbumID = 25 ;
+            album.SongID = 20 ;
+
+            Assert.AreEqual(album.ID, 42);
+            Assert.AreEqual(album.AlbumID, 25);
+            Assert.AreEqual(album.SongID, 20);
+        }
+
+
+
+        [TestMethod]
+        public void TestMethod_BandSongsEF()
+        {
+            var album = new BandSongsEF();
+
+            album.ID = 42;
+            album.BandID = 25;
+            album.SongID = 20;
+
+            Assert.AreEqual(album.ID, 42);
+            Assert.AreEqual(album.BandID, 25);
+            Assert.AreEqual(album.SongID, 20);
+        }
+
+        [TestMethod]
+        public void TestMethod_Band()
+        {
+            var band = new Band("Band_Namee");
+            Assert.AreEqual(band.Name, "Band_Namee");
+            band = new Band("Name", new System.Collections.Generic.List<Song>());
+        }
+
+        [TestMethod]
+        public void TestMethod_BandEF()
+        {
+            var band = new BandEF();
+
+            band.ID = 42;
+            band.Name = "Prodigy";
+
+            Assert.AreEqual(band.ID, 42);
+            Assert.AreEqual(band.Name, "Prodigy");
+        }
+
+
+        [TestMethod]
+        public void TestMethod_PlayListSongsEF()
+        {
+            var album = new PlayListSongsEF();
+
+            album.ID = 42;
+            album.PlayListID = 25;
+            album.SongID = 20;
+
+            Assert.AreEqual(album.ID, 42);
+            Assert.AreEqual(album.PlayListID, 25);
+            Assert.AreEqual(album.SongID, 20);
+        }
+
+        [TestMethod]
+        public void TestMethod_Playlist()
+        {
+            var playlist = new PlayList("Playlist_Namee");
+            Assert.AreEqual(playlist.Name, "Playlist_Namee");
+            playlist = new PlayList("Name", new System.Collections.Generic.List<Song>());
+        }
+
+        [TestMethod]
+        public void TestMethod_PlaylistEF()
+        {
+            var playlist = new PlayListEF();
+
+            playlist.ID = 42;
+            playlist.Name = "Prodigy";
+
+            Assert.AreEqual(playlist.ID, 42);
+            Assert.AreEqual(playlist.Name, "Prodigy");
+        }
+
+        [TestMethod]
+        public void TestMethod_Song()
+        {
+            var song = new Song
+            {
+                ID = 42,
+                Name = "",
+                Duration = default(TimeSpan),
+                Location = ""
+            };
+            Assert.AreEqual(song.Name, Song.EmptySong.Name);
+            Assert.AreEqual(song.Location, Song.EmptySong.Location);
+            Assert.AreEqual(song.Duration, Song.EmptySong.Duration);
+            Assert.AreEqual(song.Volume, Song.EmptySong.Volume);
+            Assert.AreEqual(song.ID, 42);
+            Assert.AreEqual(song.DurationString, Song.EmptySong.DurationString);
+          
+        }
+
+
+        [TestMethod]
+        public void TestMethod_Collection()
+        {
+
+            var collection = new Collection(
+                new System.Collections.Generic.List<Album>(),
+                new System.Collections.Generic.List<Band>(),
+                new System.Collections.Generic.List<PlayList>(),
+                new System.Collections.Generic.List<Song>()
+                );
+            Assert.AreEqual(collection.Albums.Count, new System.Collections.Generic.List<Album>().Count);
+            Assert.AreEqual(collection.Bands.Count, new System.Collections.Generic.List<Band>().Count);
+            Assert.AreEqual(collection.PlayLists.Count, new System.Collections.Generic.List<PlayList>().Count);
+            Assert.AreEqual(collection.Songs.Count, new System.Collections.Generic.List<Song>().Count);
+        }
+
+        [TestMethod]
+        public void TestMethod_SongList()
+        {
+            var songList = new SongList();
+            songList = new SongList(new System.Collections.Generic.List<Song>());
+            Assert.AreEqual(songList.Songs.Count, new System.Collections.Generic.List<Song> ().Count);
+     
+        }
+
+    }
+}
--- UnitTesting Rozabto/UnitTest1.cs
+++ UnitTesting Rozabto/UnitTest1.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Rozabto.Model;
 
 namespace UnitTesting_Rozabto
 {
@@ -8,38 +7,8 @@ namespace UnitTesting_Rozabto
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod_Album()
+        public void TestMethod_()
         {
-            var album = new Album("Album_Namee");
-            Assert.AreEqual(album.Name, "Album_Namee");
         }
-        [TestMethod]
-        public void TestMethod_Band()
-        {
-            var band = new Band("Band_Namee");
-            Assert.AreEqual(band.Name, "Band_Namee");
-        }
-
-        [TestMethod]
-        public void TestMethod_Playlist()
-        {
-            var playlist = new PlayList("Playlist_Namee");
-            Assert.AreEqual(playlist.Name, "Playlist_Namee");
-        }
-
-        [TestMethod]
-        public void TestMethod_Song()
-        {
-            var song = new Song
-            {
-                Name = "",
-                Duration = default(TimeSpan),
-                Location = ""
-            };
-            Assert.AreEqual(song.Name, Song.EmptySong.Name);
-            Assert.AreEqual(song.Location, Song.EmptySong.Location);
-            Assert.AreEqual(song.Duration, Song.EmptySong.Duration);
-        }
-
     }
 }
