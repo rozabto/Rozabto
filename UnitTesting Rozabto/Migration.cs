46f2cf96dbd7810e4e1ffff03a81815fd082bac4
NanislavaPavlova <35624878+NanislavaPavlova@users.noreply.github.com>
2019-04-03 18:00:24 +0300

    unit _tests

---
+++ UnitTesting Rozabto/Migration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rozabto.Migrations;
+
+namespace UnitTesting_Rozabto
+{
+    [TestClass]
+    public class Migration
+    {
+        [TestMethod]
+        public void TestMethod_test()
+        {
+            var start = new StartOfDataBase();
+            start.Down();
+            start.Up();
+            var changed = new ChangedABP();
+            changed.Down();
+            changed.Up();
+            var volume = new Volume();
+            volume.Down();
+            volume.Up();
+        }
+    }
+}
